7a24e2a5c144083ab7ff4e2a354f72073e89a0fc
Pieguy52 <vhhernandez5@mines.utep.edu>
2017-10-17 18:45:45 -0600

    BSTreeTest.c update

--- BSTreeTest.c
+++ BSTreeTest.c
@@ -18,9 +18,33 @@ int gets_n(char *s, int limit){
 int main(){
   char buf[100];
   treeNode *root = NULL;
-
-  while(gets_n(buf, 100))
-    root = insertion(root, buf);
-
-  printTree(root);
+  char ans[100];
+  
+  while(ans[0] != '9'){
+    printf("Would you like to 1: Insert names, 2: Delete names, 3: Print all names in tree, 9: exit. (1, 2, 3, 9)\n");
+    printf("Enter selection: ");
+    fgets(ans, 100, stdin);
+    switch(ans[0]){
+    case '1':
+      printf("Start inputing names: ");
+      while(gets_n(buf, 100))
+	root = insertion(root, buf);
+      break;
+    case '2':
+      printf("Insert name to delete: ");
+      while(gets_n(buf, 100))
+	root = deletion(root, buf);
+      break;
+    case '3':
+      printTree(root);
+      break;
+    case '9':
+      printf("Goodbye, closing.\n");
+      break;
+    default:
+      printf("Invalid input. Valid inputs 1, 2, 3, or 9.\n");
+      break;
+    }
+  }
+  
 }
